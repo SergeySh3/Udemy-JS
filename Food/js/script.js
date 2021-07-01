940b2a5522db23981d28b44bd8e30821e00b1bcd
Sergey Shabanov <emugrey@yandex.ru>
2021-07-01 22:01:55 +0300

    Unit 38

---
+++ Food/js/script.js
@@ -0,0 +1,54 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    //Показать картинку и текст в табе      
+    function hideTabContent () {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    //Скрыть картинку и текст в табе   
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    //Переключение табов
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);    
+                }
+            });
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+    
+
+});
