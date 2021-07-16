0d862c03dcf88019816ab7754a9dd964ebc26fd3
Sergey Shabanov <emugrey@yandex.ru>
2021-07-16 18:21:37 +0300

    Unit 56

--- Food/js/script.js
+++ Food/js/script.js
@@ -259,31 +259,35 @@ window.addEventListener('DOMContentLoaded', function() {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest(); // создаем объект запроса
-            request.open('POST', 'server.php'); // настройки объекта
-            request.setRequestHeader('Content-type', 'application/json'); //  хедер           
+                     
             const formData = new FormData(form); // тип данный что будем отправлять
 
             // передаем FormData в формате json
             const object = {}; // создаем пустой объект
             formData.forEach(function(value, key){ // перебираем FormData и все свойстава записываем в новый пустой объект
                 object[key] = value;
+            });            
+
+            // fetch-запрос на сервер
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();                 
+            })
+            .catch(() => {
+                showThanksModal(message.failure);    
+            })
+            .finally(() => {
+                form.reset();
             });
-            const json = JSON.stringify(object); // конвертируем новый объект в json
-
-            request.send(json); //делаем запрос
-
-            request.addEventListener('load', () => { //отслеживаем загрузку запроса
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();                                       
-                } else {
-                    showThanksModal(message.failure);   
-                }
-            });
-
         });
     }
 
